81623d805b587438ffb7bb79de370e8d83b3dce5
AlfieriChou <alfierichou@gmail.com>
2019-04-30 10:27:33 +0800

    rust log

---
+++ log/src/main.rs
@@ -0,0 +1,19 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use log::Level;
+
+fn main() {
+  env_logger::init();
+
+  debug!("this is a debug {}", "message");
+  error!("this is printed by default");
+
+  if log_enabled!(Level::Info) {
+    let x = 3 * 4; // expensive computation
+    info!("the answer was: {}", x);
+  }
+
+  info!("Hello World");
+}
